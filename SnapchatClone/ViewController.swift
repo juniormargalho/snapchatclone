229de2ac8cd51afd72d0104a887dbf54fbf8011c
JuniorMargalho <juniormargalho@gmail.com>
2021-04-09 14:06:47 -0300

    Initial Commit

---
+++ SnapchatClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SnapchatClone
+//
+//  Created by Nelson Antonio Barbosa Margalho Junior on 09/04/21.
+//  Copyright © 2021 JuniorMargalho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
