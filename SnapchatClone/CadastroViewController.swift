9fe43a5f008326f338849dc00621ed0a01fbc2ed
JuniorMargalho <juniormargalho@gmail.com>
2021-04-13 15:44:34 -0300

    interface de cadastro e login

---
+++ SnapchatClone/CadastroViewController.swift
@@ -0,0 +1,22 @@
+//
+//  CadastroViewController.swift
+//  SnapchatClone
+//
+//  Created by Nelson Antonio Barbosa Margalho Junior on 13/04/21.
+//  Copyright © 2021 JuniorMargalho. All rights reserved.
+//
+
+import UIKit
+
+class CadastroViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(false, animated: false)
+    }
+
+}
---
+++ SnapchatClone/EntrarViewController.swift
@@ -0,0 +1,22 @@
+//
+//  EntrarViewController.swift
+//  SnapchatClone
+//
+//  Created by Nelson Antonio Barbosa Margalho Junior on 13/04/21.
+//  Copyright © 2021 JuniorMargalho. All rights reserved.
+//
+
+import UIKit
+
+class EntrarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(false, animated: false)
+    }
+
+}
