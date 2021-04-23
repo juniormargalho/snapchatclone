df2b63aef1cf5b7b6ed58c02df11f3f89cbe0dab
JuniorMargalho <juniormargalho@gmail.com>
2021-04-23 16:34:57 -0300

    upload de imagem para o firebase

--- SnapchatClone/FotoViewController.swift
+++ SnapchatClone/FotoViewController.swift
@@ -7,16 +7,43 @@
 //
 
 import UIKit
+import FirebaseStorage
 
 class FotoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     @IBOutlet weak var imagem: UIImageView!
     @IBOutlet weak var descricao: UITextField!
+    @IBOutlet weak var botaoProximo: UIButton!
+    
     var imagePicker = UIImagePickerController()
+    
+    @IBAction func proximoPasso(_ sender: Any) {
+        self.botaoProximo.isEnabled = false
+        self.botaoProximo.setTitle("Carregando...", for: .normal)
+        
+        let armazenamento = Storage.storage().reference()
+        let imagens = armazenamento.child("imagens")
+        
+        //recupera a imagem
+        if let imagemSelecionada = imagem.image {
+            if let imagemDados = imagemSelecionada.jpegData(compressionQuality: 0.5){
+                imagens.child("imagem.jpg").putData(imagemDados, metadata: nil) { (metaDados, erro) in
+                    
+                    if erro == nil {
+                        print("sucesso")
+                        self.botaoProximo.isEnabled = true
+                        self.botaoProximo.setTitle("Próximo", for: .normal)
+                    }else {
+                        print("falha")
+                    }
+                    
+                }
+            }
+        }
+    }
     
     @IBAction func selecionarFoto(_ sender: Any) {
         imagePicker.sourceType = .savedPhotosAlbum
         present(imagePicker, animated: true, completion: nil)
-        
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
@@ -29,7 +56,6 @@ class FotoViewController: UIViewController, UIImagePickerControllerDelegate, UIN
         super.viewDidLoad()
         
         imagePicker.delegate = self
-
     }
     
 }
